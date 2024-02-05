d87c8d1156509950ae3d9f0a97684cc9671c91c2
simantunes008 <simaopfantunes15@duck.com>
2024-02-05 22:41:58 +0000

    sopa

--- TPC1/mapa.py
+++ TPC1/mapa.py
@@ -1,7 +1,16 @@
 import json
 
-
 html = """
+<!DOCTYPE html>
+<html lang "en-US">
+<head>
+    <title> Mapa</title>
+    <meta charset="utf-8">
+</head>
+<body>
+"""
+
+template = """
 <!DOCTYPE html>
 <html lang "en-US">
 <head>
@@ -15,15 +24,31 @@ file = open("mapa.json", "r", encoding="utf-8").read()
 
 result = json.loads(file)
 
-html += "<ul>"
+html += "<ul>"  
 
+listadeCidades = []
 for elem in result ["cidades"]:
-    html += f'<li>{elem ["nome"]}</li>'
+    listadeCidades.append(elem["nome"])
+    ficheiroCidade = open(f"html/{elem['nome']}.html", "w")
+    templateCidade = template
+    templateCidade += f"<h1>{elem['nome']}</h1>"
+    templateCidade += f"<h2>{elem['distrito']}</h1>"
+    templateCidade += f"<b>População: </b>{elem['população']}"
+    templateCidade += f"<br>"
+    templateCidade += f"<b>Descrição: </b>{elem['descrição']}"
+    templateCidade += "</body>"
+    templateCidade += '<h6><a href="../mapa_sorted_link.html">Voltar </h6>'
+    ficheiroCidade.write(templateCidade)
+    ficheiroCidade.close()
     
 html += "</ul>"
 
+for elem in sorted (listadeCidades):
+    html += f'<li><a href="html/{elem}.html">{elem}</a></li>'
+
+html += "</ul>"
 html += "</body>"
 
-ficheiroHtml = open ("mapa.html", "w", encoding="utf-8")
+ficheiroHtml = open ("mapa_sorted_link.html", "w", encoding="utf-8")
 ficheiroHtml.write(html)
 ficheiroHtml.close()
